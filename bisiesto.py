25023bab7ea04fa22488f0c8e8b1149770e82213
Francisco Jesús Jiménez Hidalgo <jesus@mrxuso.com>
2016-10-03 14:54:51 +0200

    documented esBisiesto function

--- bisiesto.py
+++ bisiesto.py
@@ -1,6 +1,9 @@
 '''********** CALCULA SI UN AÑO ES BISIESTO *********'''
 
 def esBisiesto(anio):
+    """int -> boolean
+    OBJ: Es el año bieisto?
+    PRE: anio >= 1582"""
     return anio % 4 == 0 and anio % 100 != 0 or anio % 400 == 0
 
 print(esBisiesto(2200))
