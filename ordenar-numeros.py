b4f6f597d7c93dffd5bc3b04f98ce03a52a7aa0b
mrxuso <fjjsus@gmail.com>
2016-10-06 13:54:21 +0200

    Añadido nombre-meses.py

--- ordenar-numeros.py
+++ ordenar-numeros.py
@@ -3,6 +3,30 @@
 
 
 def menorAMayor(i, j, k):
+    """num, num, num -> num, num, num
+        OBJ: ordena de menor a mayor
+        PRE: no hay"""
+    if i <= j:
+        if j < k:
+            ordenados = i, j, k
+        else:
+            if i < k:
+                ordenados = i, k, j
+            else:
+                ordenados = k, i, j
+    else:
+        if k <= j:
+            ordenados = k, j, i
+        else:
+            if k <= i:
+                ordenados = j, k, i
+            else:
+                ordenados = j, i, k
+
+    return ordenados
+
+
+def menorAMayorCambio(i, j, k):
     """int, int, int
     OBJ: ordena de menor a mayor
     PRE: no hay"""
@@ -22,13 +46,13 @@ def menorAMayor(i, j, k):
             i = j
             j = aux
 
-    return "(" + str(i) + "," + str(j) + "," + str(k) + ")"
+    return i,j,k
 
 
-#Probador
-print(menorAMayor(1,2,3))
-print(menorAMayor(1,3,2))
-print(menorAMayor(2,1,3))
-print(menorAMayor(2,3,1))
-print(menorAMayor(3,1,2))
-print(menorAMayor(3,2,1))
+# Probador
+print(menorAMayorCambio(1,2,3))
+print(menorAMayorCambio(1,3,2))
+print(menorAMayorCambio(2,1,3))
+print(menorAMayorCambio(2,3,1))
+print(menorAMayorCambio(3,1,2))
+print(menorAMayorCambio(3,2,1))
