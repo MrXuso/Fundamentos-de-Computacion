27318c5c5b285807014b774607ce10a3ff070366
mrxuso <fjjsus@gmail.com>
2016-10-28 20:29:37 +0200

    Añadida función monedasOptimas

--- dinero.py
+++ dinero.py
@@ -3,15 +3,39 @@
  *            FRANCISCO JESÚS JIMÉNEZ HIDALGO             *
 '''******************************************************"""
 
-monedasCirculacion = 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500
+monedasCirculacion = 0.01, 0.02, 0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0, 20.0, 50.0, 100.0, 200.0, 500.0
 
 def dineroMonedero(monedero):
-    """tuple -> float
-    OBJ: Dinero total en el monedero"""
+    """------------------------------------------+++
+    +++tuple -> float                            +++
+    ---OBJ: Dinero total en el monedero          +++
+    ---PRE: monedero: tupla de enteros positivos +++
+    +++------------------------------------------"""
     total = 0
     for i in range(0,len(monedasCirculacion)):
         total += monedasCirculacion[i] * monedero[i]
 
     return total
 
-print(dineroMonedero((0,0,0,0,0,0,0,0,0,0,0,0,0,0,1)))
+
+def enMonedasOptimas(dinero):
+    """------------------------------------------+++
+    +++float -> tuple                            +++
+    +++OBJ: tupla con el mínimo número de monedas+++
+    +++     que componen una cantidad de dinero  +++
+    +++PRE: mínima subdivisión de dinero 0,01    +++
+    +++------------------------------------------"""
+
+    monedasOptimas = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+    for i in range(len(monedasCirculacion)-1, -1, -1):
+        if dinero / monedasCirculacion[i] >= 1:
+            monedasOptimas[i] = int(dinero // monedasCirculacion[i])
+            dinero = round(dinero - monedasCirculacion[i]*(dinero // monedasCirculacion[i]), 2)
+
+    return tuple(monedasOptimas)
+
+
+# PROBADOR
+for dinero in monedasCirculacion:
+    print(enMonedasOptimas(dinero))
